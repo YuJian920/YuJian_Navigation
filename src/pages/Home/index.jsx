00102ff79c0b6af26fa470c980c7916c8ade170e
YuJian920 <HAO52013141@Gmail.com>
2022-02-09 21:47:09 +0800

    搜素引擎切换

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -2,15 +2,32 @@ import React, { useState } from "react";
 import { getImageUrl } from "../../utils";
 import "./index.less";
 
+const TypeDict = [
+  {
+    type: "Google",
+    imgPath: "google.png",
+    searchPath: "https://www.google.com/search?q=",
+  },
+  {
+    type: "Baidu",
+    imgPath: "baidu.png",
+    searchPath: "https://www.baidu.com/s?&wd=",
+  },
+];
+
 const Home = () => {
   const [searchWord, setSearchWord] = useState("");
-  // const [searchType, setSearchType] = useState("https://www.google.com/search?q=")
+  const [searchType, setSearchType] = useState(0);
 
   /** 搜索事件 */
   const _searchEvent = (e) => {
     if (e.keyCode !== 13) return;
-    const searchType = "https://www.google.com/search?q=";
-    window.location.href = `${searchType}${encodeURIComponent(searchWord)}`;
+    const serchURL = TypeDict[searchType].searchPath;
+    window.location.href = `${serchURL}${encodeURIComponent(searchWord)}`;
+  };
+
+  const _onSearhChange = (value) => {
+    TypeDict[value].type;
   };
 
   return (
@@ -20,7 +37,7 @@ const Home = () => {
           <div className="searchBox__iconBox flexEle">
             <img
               className="searchBox__iconBox-icon"
-              src={getImageUrl("google.png")}
+              src={getImageUrl(TypeDict[searchType].imgPath)}
             />
           </div>
           <div className="searchBox__inputBox flexEle">
@@ -32,7 +49,33 @@ const Home = () => {
               onChange={(e) => setSearchWord(e.target.value)}
             />
           </div>
-          <div className="searchBox__searchPopup"></div>
+          <div
+            className="searchBox__searchPopup fadeIn animated"
+            style={{ display: "none" }}
+          >
+            <div
+              className="searchBox__searchPopup-item flexEle"
+              onClick={() => setSearchType(0)}
+            >
+              <img
+                className="itemImg"
+                src={getImageUrl("google.png")}
+                alt="Google.com"
+              />
+              <span className="itemTitle">Google</span>
+            </div>
+            <div
+              className="searchBox__searchPopup-item flexEle"
+              onClick={() => setSearchType(1)}
+            >
+              <img
+                className="itemImg"
+                src={getImageUrl("baidu.png")}
+                alt="Baidu.com"
+              />
+              <span className="itemTitle">百度</span>
+            </div>
+          </div>
         </div>
       </div>
     </>
